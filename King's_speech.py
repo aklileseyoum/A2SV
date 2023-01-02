81f265e3b7642d30c3dab93484326cb9d89447e8
aklileseyoum <75662851+aklileseyoum@users.noreply.github.com>
2023-01-02 17:27:52 +0300

    Create King's_speech.py

---
+++ King's_speech.py
@@ -0,0 +1,8 @@
+    no_of_words = int(input())
+    for i in range(no_of_words):
+        word = input()
+        print(word[:2],end = "")
+        print("... ",end="")
+        print(word,end="")
+        print("?")
+

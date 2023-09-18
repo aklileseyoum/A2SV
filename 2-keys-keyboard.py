e0f8f958915db1d57b7e42a9eb189fcb8291f82e
aklileseyoum <75662851+aklileseyoum@users.noreply.github.com>
2023-09-18 10:37:54 +0300

    added 2-keys-keyboard.python

--- 2-keys-keyboard.py
+++ 2-keys-keyboard.py
@@ -1,26 +1,12 @@
 class Solution:
-    def isPrime(self, num):
-        d = 2
-        while d * d <= num:
-            if num % d == 0:
-                return False
-            d += 1
-        return True
-        
     def minSteps(self, n: int) -> int:
-        if n == 1:
-            return 0
-
-        if self.isPrime(n):
-            return n
-        
-        answer = 0
-        divisor = 2
+        ans = 0
+        divider = 2
         while n > 1:
-            if n % divisor != 0:
-                divisor += 1
-            else:
-                answer += divisor
-                n = n // divisor
+            if n % divider:
+                divider += 1
+                continue
+            n /= divider
+            ans += divider
 
-        return answer
+        return ans

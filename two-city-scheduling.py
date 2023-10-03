c597e0d3e2f37478a67a779af0c1106d5cdde388
aklileseyoum <75662851+aklileseyoum@users.noreply.github.com>
2023-10-03 17:08:17 +0300

    added two-city-scheduling.python

---
+++ two-city-scheduling.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        ranges = []
+        for idx in range(len(costs)):
+            ranges.append([(costs[idx][0] - costs[idx][1]), idx])
+        ranges.sort()
+        
+        ans = 0
+        length = len(costs)
+        for idx in range(len(ranges)):
+            val, i = ranges[idx]
+            if idx < length / 2:
+                ans += costs[i][0]
+            else:
+                ans += costs[i][1]
+
+        return ans

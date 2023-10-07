39a3cdd110c8ff2d63f69e66a38151d7497c43a8
aklileseyoum <75662851+aklileseyoum@users.noreply.github.com>
2023-10-07 13:41:04 +0300

    added k-th-smallest-in-lexicographical-order.python

---
+++ k-th-smallest-in-lexicographical-order.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        if n < k or k < 1:
+            return 0
+        elif n < 10:
+            return k
+        cur = 1
+        k -= 1
+        while k > 0:
+            step, first, last = 0, cur, cur + 1
+            while first <= n:
+                step += min(n + 1, last) - first
+                first *= 10
+                last *= 10
+
+            if step <= k:
+                cur += 1
+                k -= step
+            else:
+                cur *= 10
+                k -= 1
+        return cur

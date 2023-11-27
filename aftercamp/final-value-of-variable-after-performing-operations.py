02d2a43c9c868a239f7749b6a33ba1491f8726f7
aklileseyoum <75662851+aklileseyoum@users.noreply.github.com>
2023-11-27 23:07:31 +0300

    Added solution for Final Value of Variable After Performing Operations

---
+++ aftercamp/final-value-of-variable-after-performing-operations.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        """
+        :type operations: List[str]
+        :rtype: int
+        """
+        x = 0
+        for operation in operations:
+            if operation[0] == '-' or operation[2] == '-':
+                x -= 1
+            else:
+                x += 1
+
+        return x

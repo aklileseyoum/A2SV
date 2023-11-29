0d1e323ac88569e229fbe17caa7541f47473dd6b
aklileseyoum <75662851+aklileseyoum@users.noreply.github.com>
2023-11-29 14:02:39 +0300

    Added solution for Backspace String Compare

---
+++ aftercamp/backspace-string-compare.py
@@ -0,0 +1,20 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        s_stack = []
+        t_stack = []
+        for letter in s:
+            if letter == "#":
+                if s_stack:
+                    s_stack.pop()
+            else:
+                s_stack.append(letter)
+
+        for letter in t:
+            if letter == "#":
+                if t_stack:
+                    t_stack.pop()
+            else:
+                t_stack.append(letter)
+
+        return (s_stack == t_stack)
+        
